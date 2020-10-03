87076c12bf2b6df54b39df0669069a5c1eb3fa51
Pittis Thomas <thomas.pittis@ens.uvsq.fr>
2020-10-03 15:47:12 +0200

    MAJ des fichiers pour questions d'après
    Remplissage des deux fichiers .java pour afficher une fraction entre deux nombres en arg.

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,19 @@
 
 public class Fraction {
+	private int x;
+	private int y;
+	
+	public Fraction(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		System.out.println(this.x + "/" + this.y);
+		 
+		return this.x + "/" + this.y;
+	}
+	
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
 	
 	public static void main (String[] args) {
 		System.out.println("Affichage simple");
+		
+		Fraction fraction = new Fraction(1,1);
+		fraction.toString();
 	}
 
 }
